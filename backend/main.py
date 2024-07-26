4f906a21ef6167f0e75bf70e827650b1ad581c1a
Kaide Wu <kaidewu@outlook.es>
2024-07-26 23:33:08 +0200

    Implement fire package to execute the FastAPI app

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 import uvicorn
+import fire
 from settings.settings import settings
 
-if __name__ == "__main__":
+
+def main() -> None:
+    print(f"Listening on {settings.HOST}:{settings.SERVICE_PORT}")
     uvicorn.run(
         app="app:app",
         host=settings.HOST,
         port=settings.SERVICE_PORT,
-        reload=True,
         use_colors=True
     )
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
